2fea4282833564122ecf12b391e2eba0ba313b6b
Quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2022-07-07 23:05:49 +0200

    Checking the Emacs' type before doing things with scrollbar.

--- lisp/shortcuts.el
+++ lisp/shortcuts.el
@@ -5,6 +5,16 @@
 ;; global emacs configuration
 ;;
 
+;; Some function that will get binded to keys
+(defun pop-the-shell ()
+  "This function will, once called, pop a shell to a new bottom window."
+  (interactive)
+  (let ((w (split-window-below 2)))
+    (select-window w)
+    (shell))
+  (switch-to-buffer w))
+
+
 ;; Define keyboard shortcuts
 (global-set-key (kbd "M-p") 'pop-to-buffer)
 (global-set-key (kbd "M-o") 'find-file)
@@ -15,3 +25,4 @@
 (global-set-key (kbd "M-Z") 'recover-this-file)
 (global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 (global-set-key (kbd "M-b") 'counsel-find-file)
+(global-set-key (kbd "M-j") 'pop-the-shell)
--- lisp/ui.el
+++ lisp/ui.el
@@ -2,10 +2,13 @@
 
 (setq inhibit-startup-message t)
 
-(scroll-bar-mode -1) ; Disable visible scrollbar.
-(tool-bar-mode -1)   ; Disable the toolbar.
-(tooltip-mode -1)    ; Disable the tooltips.
-(set-fringe-mode 10) ; Give some breathing room.
+(unless (not (window-system))
+  (scroll-bar-mode -1)  ; Disable visible scrollbar.
+  (tool-bar-mode -1)    ; Disable the toolbar.
+  (tooltip-mode -1)     ; Disable the tooltips.
+  (set-fringe-mode 10)) ; Give some breathing room.
+
+(menu-bar-mode -1)
 
 (load "font.el")
 
