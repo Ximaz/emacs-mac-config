294c739c05f571e0e22a62871147743314bb52eb
Quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2022-07-07 17:14:22 +0200

    Removing sym links.

--- init.el
+++ init.el
@@ -1,1 +1,15 @@
-/Users/user/.emacs.d/init.el
+;;
+;; XIMAZ CUSTOM EMACS, 2022
+;; custom-emacs
+;; File description:
+;; autoload custom configuration on startup
+;;
+
+(add-to-list 'load-path "~/.emacs.d/lisp")
+
+(load "ui.el")
+(load "pm.el")
+(load "shortcuts.el")
+(load "epitech.el")
+
+(setq auto-save-default nil)
--- lisp
+++
@@ -1,1 +0,0 @@
-/Users/user/.emacs.d/lisp
