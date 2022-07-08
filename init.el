ccb0e4df66926f6822ddfdaa399485002cf14f2b
Quatrecentquatre-404 <obvious.ly.dev@gmail.com>
2022-07-08 17:27:30 +0200

    Fragmenting packages and adding wakatime.

--- init.el
+++ init.el
@@ -12,6 +12,7 @@
 (load "ui.el")
 (load "epitech.el")
 (load "backup.el")
+(load "personal.el")
 
 (setq auto-save-default nil)
 (custom-set-variables
@@ -27,8 +28,9 @@
  '(custom-enabled-themes '(brin))
  '(custom-safe-themes
    '("5f128efd37c6a87cd4ad8e8b7f2afaba425425524a68133ac0efd87291d05874" "7a424478cb77a96af2c0f50cfb4e2a88647b3ccca225f8c650ed45b7f50d9525" "9b59e147dbbde5e638ea1cde5ec0a358d5f269d27bd2b893a0947c4a867e14c1" default))
+ '(mouse-wheel-progressive-speed nil)
  '(package-selected-packages
-   '(doom-themes multiple-cursors visual-fill-column org-bullets evil-collection magit evil-magit all-the-icons-dired all-the-icons sublime-themes helpful ivy-rich which-key rainbow-delimiters use-package doom-modeline counsel command-log-mode))
+   '(wakatime-mode doom-themes multiple-cursors visual-fill-column org-bullets evil-collection magit evil-magit all-the-icons-dired all-the-icons sublime-themes helpful ivy-rich which-key rainbow-delimiters use-package doom-modeline counsel command-log-mode))
  '(show-paren-mode t))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
--- lisp/pm.el
+++ lisp/pm.el
@@ -13,141 +13,16 @@
 (unless (package-installed-p 'use-package)
    (package-install 'use-package))
 
-(require 'use-package)
-(setq use-package-always-ensure t)
-
-(use-package command-log-mode)
-
-(use-package ivy
-  :diminish
-  :bind (("C-s" . swiper)
-         :map ivy-minibuffer-map
-         ("TAB" . ivy-alt-done)	
-         ("C-l" . ivy-alt-done)
-         ("C-j" . ivy-next-line)
-         ("C-k" . ivy-previous-line)
-         :map ivy-switch-buffer-map
-         ("C-k" . ivy-previous-line)
-         ("C-l" . ivy-done)
-         ("C-d" . ivy-switch-buffer-kill)
-         :map ivy-reverse-i-search-map
-         ("C-k" . ivy-previous-line)
-         ("C-d" . ivy-reverse-i-search-kill))
-  :config
-  (ivy-mode 1))
-
-(use-package doom-modeline
-  :ensure t
-  :init (doom-modeline-mode 1)
-  :custom ((doom-modeline-height 15)))
-
-(use-package doom-themes
-  :ensure t
-  :config
-  (setq doom-themes-enable-bold t    ; if nil, bold is universally disabled
-        doom-themes-enable-italic t) ; if nil, italics is universally disabled
-  (load-theme 'doom-palenight t)
-  (doom-themes-visual-bell-config)
-  )
-
-(use-package rainbow-delimiters
-  :hook (prog-mode . rainbow-delimiters-mode))
-
-(use-package which-key
-  :init (which-key-mode)
-  :diminish which-key-mode
-  :config
-  (setq which-key-idle-delay 0.3))
-
-(use-package ivy-rich
-  :init (ivy-rich-mode 1))
-
-(use-package counsel
-  :bind (("M-x" . counsel-M-x)
-         ("C-x b" . counsel-ibuffer)
-         ("C-x C-f" . counsel-find-file)
-         ("M-p" . counsel-switch-buffer)
-         :map minibuffer-local-map
-         ("C-r" . counsel-minibuffer-history)))
-
-(use-package helpful
-  :custom
-  (counsel-describe-function-function #'helpful-callable)
-  (counsel-describe-variable-function #'helpful-variable)
-  :bind
-  ([remap describe-function] . counsel-describe-function)
-  ([remap describe-command] . helpful-command)
-  ([remap describe-variable] . counsel-describe-variable)
-  ([remap describe-key] . helpful-key))
-
-(use-package all-the-icons
-  :if (display-graphic-p)
-  :commands all-the-icons-install-fonts
-  :init
-  (unless (find-font (font-spec :name "all-the-icons"))
-    (all-the-icons-install-fonts t)))
-
-(use-package all-the-icons-dired
-  :if (display-graphic-p)
-  :hook (dired-mode . all-the-icons-dired-mode))
-
-(use-package magit
-  :custom (magit-display-buffer-function #'magit-display-buffer-same-window-except-diff-v1))
-
-
-(defun efs/org-font-setup ()
-  ;; Replace list hyphen with dot
-  (font-lock-add-keywords 'org-mode
-                          '(("^ *\\([-]\\) "
-                             (0 (prog1 () (compose-region (match-beginning 1) (match-end 1) "•"))))))
-
-  ;; Set faces for heading levels
-  (dolist (face '((org-level-1 . 1.2)
-                  (org-level-2 . 1.1)
-                  (org-level-3 . 1.05)
-                  (org-level-4 . 1.0)
-                  (org-level-5 . 1.1)
-                  (org-level-6 . 1.1)
-                  (org-level-7 . 1.1)
-                  (org-level-8 . 1.1)))
-    (set-face-attribute (car face) nil :font "Cantarell" :weight 'regular :height (cdr face)))
-
-  ;; Ensure that anything that should be fixed-pitch in Org files appears that way
-  (set-face-attribute 'org-block nil :foreground nil :inherit 'fixed-pitch)
-  (set-face-attribute 'org-code nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-table nil   :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-verbatim nil :inherit '(shadow fixed-pitch))
-  (set-face-attribute 'org-special-keyword nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-meta-line nil :inherit '(font-lock-comment-face fixed-pitch))
-  (set-face-attribute 'org-checkbox nil :inherit 'fixed-pitch))
-
-(use-package org
-  :hook (org-mode . efs/org-mode-setup)
-  :config
-  (setq org-ellipsis " ▾")
-  (efs/org-font-setup))
-
-(use-package org-bullets
-  :after org
-  :hook (org-mode . org-bullets-mode)
-  :custom
-  (org-bullets-bullet-list '("◉" "○" "●" "○" "●" "○" "●")))
-
-(defun efs/org-mode-visual-fill ()
-  (setq visual-fill-column-width 100
-        visual-fill-column-center-text t)
-  (visual-fill-column-mode 1))
-
-(use-package visual-fill-column
-  :hook (org-mode . efs/org-mode-visual-fill))
-
-(use-package multiple-cursors
-  :bind (("C-M-<up>" . mc/mark-previous-like-this)
-         ("M-C-<up>" . mc/mark-previous-like-this)
-         ("C-M-<down>" . mc/mark-next-like-this)
-         ("M-C-<down>" . mc/mark-next-like-this)
-         ("<escape>" . mc/keyboard-quit)
-         ("<mouse-1>" . mc/keyboard-quit)
-         ("M-S-<mouse-1>" . mc/toggle-cursor-on-click)
-         ))
-
+(load "packages/doom-modeline.el")
+(load "packages/doom-themes.el")
+(load "packages/all-the-icons.el")
+(load "packages/all-the-icons-dired.el")
+(load "packages/rainbow-delimiters.el")
+(load "packages/which-key.el")
+(load "packages/helpful.el")
+(load "packages/multiple-cursors.el")
+(load "packages/counsel.el")
+(load "packages/ivy.el")
+(load "packages/ivy-rich.el")
+(load "packages/magit.el")
+(load "packages/wakatime.el")
